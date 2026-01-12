16027e815a12a5587ffcf5cfb1d41c7418ab4f01
omar dawood <ahmaddawoud505@gmail.com>
2026-01-12 17:17:44 +0200

    workingg

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// main.dart
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -34,10 +34,8 @@ class AIHelmetApp extends StatelessWidget {
     User? user = FirebaseAuth.instance.currentUser;
 
     if (user != null) {
-      // ✅ User already logged in
       signedIn = true;
     } else {
-      // ❌ Not logged in
       signedIn = false;
     }
 
